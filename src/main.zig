e5af0bbf0e0c8edc7115fd78c5d0e78d2a006430
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2025-02-25 23:43:55 +0000

    add sbi console output

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 const __bss = @extern([*]u8, .{ .name = "__bss" });
 const __bss_end = @extern([*]u8, .{ .name = "__bss_end" });
 const __stack_top = @extern([*]u8, .{ .name = "__stack_top" });
@@ -16,5 +18,70 @@ export fn kernel_main() void {
     slice.ptr = __bss;
     slice.len = @intFromPtr(__bss_end) - @intFromPtr(__bss);
     @memset(slice, 0);
-    while (true) {}
+
+    log("Hello: {d}", .{69});
+
+    wfi();
+}
+
+fn wfi() void {
+    asm volatile ("wfi");
+}
+
+const sbiret = extern struct {
+    @"error": u32,
+    value: u32,
+};
+
+fn sbi_call(
+    arg0: u32,
+    arg1: u32,
+    arg2: u32,
+    arg3: u32,
+    arg4: u32,
+    arg5: u32,
+    fid: u32,
+    eid: u32,
+) sbiret {
+    asm volatile (
+        \\ecall
+        :
+        : [arg1] "{a0}" (arg0),
+          [arg2] "{a1}" (arg1),
+          [arg3] "{a2}" (arg2),
+          [arg4] "{a3}" (arg3),
+          [arg5] "{a4}" (arg4),
+          [arg6] "{a5}" (arg5),
+          [arg7] "{a6}" (fid),
+          [arg8] "{a7}" (eid),
+    );
+    // zig does not allow multliple returns from assembly
+    // read a0 and a1 manually
+    const a0 = asm volatile ("mv %[ret], a0"
+        : [ret] "={a0}" (-> u32),
+    );
+    const a1 = asm volatile ("mv %[ret], a1"
+        : [ret] "={a1}" (-> u32),
+    );
+    return .{ .@"error" = a0, .value = a1 };
+}
+
+fn sbi_put_char(char: u8) void {
+    _ = sbi_call(@intCast(char), 0, 0, 0, 0, 0, 0, 1);
+}
+
+const ConsoleWriter = std.io.GenericWriter(void, error{}, console_write);
+fn console_write(_: void, string: []const u8) !usize {
+    for (string) |c| {
+        sbi_put_char(c);
+    }
+    return string.len;
+}
+
+fn log(
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const writer: ConsoleWriter = .{ .context = {} };
+    writer.print(format ++ "\n", args) catch unreachable;
 }
